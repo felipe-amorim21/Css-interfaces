34facacd63d629e4c6b49307daf2636ebfa49640
Felipe <felipe21fab@gmail.com>
2023-07-29 19:15:01 -0300

    Finished script for changing images with button

---
+++ ImageSlider/script.js
@@ -0,0 +1,47 @@
+const buttons = document.querySelectorAll("button")
+const images = document.getElementsByTagName("li")
+
+
+function changeSlider(btn){
+    if (btn.id === "next"){
+        for(i = 0; i< images.length; i++){
+            if(images[i].className == "active"){
+                if(i == 3){
+                    images[i].classList.remove("active")
+                    images[i].classList.add("no-active")
+                    images[i-3].classList.remove("no-active")
+                    images[i-3].classList.add("active")
+                    break;
+                }
+                images[i].classList.remove("active")
+                images[i].classList.add("no-active")
+                images[i+1].classList.remove("no-active")
+                images[i+1].classList.add("active")
+                break
+            }
+    }
+}
+    if (btn.id === "previous"){
+        for(i = 0; i< images.length; i++){
+            if(images[i].className == "active"){
+                if(i == 0){
+                    images[i].classList.remove("active")
+                    images[i].classList.add("no-active")
+                    images[i + 3].classList.remove("no-active")
+                    images[i + 3].classList.add("active")
+                    break;
+                }
+                images[i].classList.remove("active")
+                images[i].classList.add("no-active")
+                images[i-1].classList.remove("no-active")
+                images[i-1].classList.add("active")
+                break
+            }
+    }
+    }
+}
+
+buttons.forEach(btn => btn.addEventListener("click", e => changeSlider(btn)))
+
+
+
